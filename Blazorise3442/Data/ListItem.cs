77cc548d4aedc773fc809991cf483af9a84032a0
hillin <hillinsilence@gmail.com>
2022-02-15 10:43:52 +0800

    initial commit

---
+++ Blazorise3442/Data/ListItem.cs
@@ -0,0 +1,3 @@
+﻿namespace Blazorise3442.Data;
+
+public record ListItem(string Name, string State, string City);
---
+++ Blazorise3442/Program.cs
@@ -0,0 +1,24 @@
+using Blazorise;
+using Blazorise.Bootstrap5;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddBlazorise().AddBootstrap5Providers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Error");
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
